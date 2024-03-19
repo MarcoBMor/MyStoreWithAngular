f7b56f687c6e8bfde78a59edd2cfe27551959f28
LAPTOP-1V25V6ST\marco <marcoberuetmorelli@gmail.com>
2024-03-19 12:42:13 +0100

    Truncate pipe and responsive text improve

--- src/app/views/card/card.component.ts
+++ src/app/views/card/card.component.ts
@@ -2,11 +2,12 @@ import { CurrencyPipe } from '@angular/common';
 import { IProduct } from './../../models/product.model';
 import { Component, inject, Input } from '@angular/core';
 import { Router } from '@angular/router';
+import { TruncatePipe } from '../../truncate.pipe';
 
 @Component({
   selector: 'app-card',
   standalone: true,
-  imports: [CurrencyPipe],
+  imports: [CurrencyPipe, TruncatePipe],
   templateUrl: './card.component.html',
   styleUrl: './card.component.css'
 })
@@ -20,5 +21,7 @@ export class CardComponent {
   navigate(id?: number): void{
     this._router.navigate(['/products', id]);
   }
+
+  
 
 }
